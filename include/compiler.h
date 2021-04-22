adff82a300a87b18f79d11c1d7b9c98f6fed88e5
wpatty <wpatty@wavious.com>
2021-04-22 16:20:47 -0700

    Common Includes
    Fixes:
      - None
    
    Features:
      - WDDR Memory Map
      - Common Return Types
      - Compiler Macros

---
+++ include/compiler.h
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2021 Wavious LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef _COMPILER_H_
+#define _COMPILER_H_
+
+#define __UNUSED__          __attribute__((unused))
+#define __INTERRUPT__       __attribute__((interrupt))
+#define __FALLTHROUGH__     __attribute__((fallthrough))
+#define __PACKED__          __attribute__((__packed__))
+#define __ALIGNED_4__       __attribute__((aligned(4)))
+#define __ALIGNED_8__       __attribute__((aligned(8)))
+#define __O1__              __attribute__((optimize("O1")))
+#define __O2__              __attribute__((optimize("O2")))
+#define __O3__              __attribute__((optimize("O3")))
+
+#endif /* _COMPILER_H_ */
