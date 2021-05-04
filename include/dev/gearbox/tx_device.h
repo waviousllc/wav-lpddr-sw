4dbd1695a1f20a19a404e7a0ceb80bc52c29984a
wpatty <wpatty@wavious.com>
2021-05-04 11:24:26 -0700

    Gearbox
    Fixes:
      - None
    
    Features:
      - Gearbox device and driver

---
+++ include/dev/gearbox/tx_device.h
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2021 Wavious LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef _GEARBOX_TX_DEV_H_
+#define _GEARBOX_TX_DEV_H_
+
+#include <gearbox/device.h>
+
+/** @brief Alias for gearbox_t since identical structure */
+typedef gearbox_dev_t tx_gearbox_dev_t;
+
+#endif /* _GEARBOX_TX_DEV_H_ */
