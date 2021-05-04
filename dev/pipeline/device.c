7f626574a2d4a8b338f678c0870cd2fb566a656e
wpatty <wpatty@wavious.com>
2021-05-04 14:52:57 -0700

    TX Bit Pipeline
    Fixes:
      - None
    
    Features:
      - TX Bit Pipeline device and driver

---
+++ dev/pipeline/device.c
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2021 Wavious LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <pipeline/device.h>
+#include <pipeline/driver.h>
+
+void pipeline_bit_init(pipeline_bit_dev_t *pipeline,
+                       uint32_t base,
+                       wddr_slice_type_t slice_type,
+                       wddr_rank_t rank,
+                       uint8_t bit)
+{
+    egress_ana_init_reg_if(&pipeline->egress_ana, base, slice_type, rank, bit);
+    egress_dig_init_reg_if(&pipeline->egress_dig, base, slice_type, rank, bit);
+    sdr_pipeline_init_reg_if(&pipeline->sdr, base, slice_type, rank, bit);
+    ddr_pipeline_init_reg_if(&pipeline->ddr, base, slice_type, rank, bit);
+    qdr_pipeline_init_reg_if(&pipeline->qdr, base, slice_type, rank, bit);
+}
