a199c35e9a7dbab89de822e930502812afdeccff
wpatty <wpatty@wavious.com>
2021-05-04 10:43:25 -0700

    Bug Fixes
    Fixes:
      - Fixed issue where vref and zqcal driver lib names were swapped
      - Fixed issue where LPDE device includes where missing
      - Fixed issue where PMON driver code was missing an include
      - Fixed issue where PMON driver code used bad variable name
    
    Features:
      - None

---
+++ include/dev/lpde/tx_device.h
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2021 Wavious LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef _LPDE_TX_DEV_H_
+#define _LPDE_TX_DEV_H_
+
+#include <stdint.h>
+#include <lpde/device.h>
+#include <wddr/phy_defs.h>
+
+/**
+ * @brief   Transmit (TX) LPDE Initalization
+ *
+ * @details Initializes TX LPDE device at device level.
+ *
+ * @param[in]   tx_lpde     pointer to TX LPDE device.
+ * @param[in]   base        base address of TX LPDE device.
+ * @param[in]   slice_type  slice type of TX LPDE device.
+ * @param[in]   rank        rank of TX LPDE device.
+ * @param[in]   bit         bit index fo TX LPDE device.
+ *
+ * @return      void
+ */
+void tx_lpde_init(lpde_dev_t *tx_lpde,
+                  uint32_t base,
+                  wddr_slice_type_t slice_type,
+                  wddr_rank_t rank,
+                  uint8_t bit);
+
+#endif /* _LPDE_TX_DEV_H_ */
