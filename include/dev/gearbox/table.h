10c04c9fff75992e715a56a82d9271e64ffd230b
wpatty <wpatty@wavious.com>
2021-05-04 11:24:26 -0700

    Gearbox
    Fixes:
      - None
    
    Features:
      - Gearbox device and driver

---
+++ include/dev/gearbox/table.h
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2021 Wavious LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef _GEARBOX_TABLE_H_
+#define _GEARBOX_TABLE_H_
+
+/**
+ * @brief   Gearbox Data Mode Enumerations
+ */
+typedef enum gb_data_mode_t
+{
+    DGB_1TO1_HF,
+    DGB_2TO1_HF,
+    DGB_2TO1_IR,
+    DGB_4TO1_LF,
+    DGB_4TO1_HF,
+    DGB_4TO1_IR,
+    DGB_8TO1_LF,
+    DGB_8TO1_HF
+} gb_data_mode_t;
+
+/**
+ * @brief   Gearbox Write Mode Enumerations
+ */
+typedef enum gb_write_mode_t
+{
+    WGB_1TO1,
+    WGB_2TO1,
+    WGB_4TO1,
+    WGB_8TO1,
+    WGB_2TO2,
+    WGB_4TO2,
+    WGB_8TO2,
+    WGB_4TO4,
+    WGB_8TO4,
+    WGB_8TO8,
+    WGB_16TO8
+} gb_write_mode_t;
+
+/**
+ * @brief   Gearbox FIFO Mode Enumerations
+ */
+typedef enum gb_fifo_mode_t
+{
+    FGB_1TO1,
+    FGB_1TO2,
+    FGB_1TO4,
+    FGB_1TO8,
+    FGB_2TO2,
+    FGB_2TO4,
+    FGB_2TO8,
+    FGB_4TO4,
+    FGB_4TO8,
+    FGB_8TO8,
+    FGB_8TO16
+} gb_fifo_mode_t;
+
+/**
+ * @brief   Gearbox WCK Mode Enumerations
+ */
+typedef enum gb_wck_mode_t
+{
+    // TODO: What does 0 and 1 actually mean?
+    GB_WCK_MODE_0,
+    GB_WCK_MODE_1
+} gb_wck_mode_t;
+
+#endif /* _GEARBOX_TABLE_H_ */
---
+++ include/drivers/gearbox/rx_driver.h
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2021 Wavious LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef _GEARBOX_RX_DRIVER_H_
+#define _GEARBOX_RX_DRIVER_H_
+
+#include <gearbox/rx_table.h>
+#include <gearbox/rx_device.h>
+#include <wddr/phy_defs.h>
+
+/**
+ * @brief   Receive (RX) Gearbox Initialization Register Interface
+ *
+ * @details Initializes RX Gearbox device at register interface level.
+ *
+ * @note    Slice type should be given as CA or DQ, which implies CK and
+ *          DQS, respectively.
+ *
+ * @param[in]   gb          pointer to RX Gearbox device to initialize.
+ * @param[in]   base        base address of RX Gearbox device.
+ * @param[in]   slice_type  slice type of RX Gearbox to initialize.
+ *
+ * @return      void
+ */
+void rx_gb_init_reg_if(rx_gearbox_dev_t *gb,
+                       uint32_t base,
+                       wddr_slice_type_t slice_type);
+
+/**
+ * @brief   Receive (RX) Gearbox Set Mode Set Register (MSR) Register Interface
+ *
+ * @details Configures RX Gearbox device for given mode set register value.
+ *
+ * @note    Slice type should be given as CA or DQ, which implies CK and
+ *          DQS, respectively.
+ *
+ * @param[in]   gb          pointer to RX Gearbox device.
+ * @param[in]   msr         MSR value to configure.
+ * @param[in]   slice_type  slice type of RX Gearbox.
+ *
+ * @return      void
+ */
+void rx_gb_set_msr_reg_if(rx_gearbox_dev_t *gb,
+                          wddr_msr_t msr,
+                          wddr_slice_type_t slice_type);
+
+/**
+ * @brief   Receive (RX) Gearbox Set Mode Register Interface
+ *
+ * @details Configures RX Gearbox device for given mode via CSR.
+ *
+ * @note    Slice type should be given as CA or DQ, which implies CK and
+ *          DQS, respectively.
+ *
+ * @param[in]   gb          pointer to RX Gearbox device.
+ * @param[in]   slice_type  slice type of RX Gearbox.
+ * @param[in]   cfg         pointer to RX gearbox configuration.
+ *
+ * @return      void
+ */
+void rx_gb_set_mode_reg_if(rx_gearbox_dev_t *gb,
+                           wddr_slice_type_t slice_type,
+                           rx_gb_cfg_t *cfg);
+
+#endif /* _GEARBOX_RX_DRIVER_H_ */
